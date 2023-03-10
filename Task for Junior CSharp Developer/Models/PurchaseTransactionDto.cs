28c000fdf6a0ef16f103963c037c94a60e4a227e
Firuz <razackoff@gmail.com>
2023-03-10 10:00:46 +0500

    Входные данные

---
+++ Task for Junior CSharp Developer/Models/PurchaseTransactionDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_for_Junior_CSharp_Developer.Models
+{
+    public class PurchaseTransactionDto
+    {
+        public string productName { get; set; }
+        public decimal productCost { get; set; }
+        public string phoneNumber { get; set; }
+        public int installmentRange { get; set; }
+    }
+}
