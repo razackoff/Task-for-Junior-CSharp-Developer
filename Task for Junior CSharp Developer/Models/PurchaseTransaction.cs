6de8cdd63af669f598cd12996d4f8b97792026a1
Firuz <razackoff@gmail.com>
2023-03-10 13:43:46 +0500

    Добавить сервис вычисления суммы и сервис отправки смс.

--- Task for Junior CSharp Developer/Models/PurchaseTransaction.cs
+++ Task for Junior CSharp Developer/Models/PurchaseTransaction.cs
@@ -8,6 +8,23 @@
         public int InstallmentRange { get; set; }
         public decimal ProductCost { get; set; }
         public decimal summa { get; set; }
-
+        
+        public void Handle()
+        {
+            InstallmentHandler installmentHandler = new InstallmentHandler();
+            installmentHandler.price = ProductCost;
+            installmentHandler.installmentRange = InstallmentRange;
+            installmentHandler.productСategory = ProductСategory;
+            summa = installmentHandler.GetInstallment();
+            
+            SmsSender smsSender = new SmsSender();
+            smsSender.PhoneNumber = PhoneNumber;
+            smsSender.Sms = $"Transaction ID = {TransactionID} \n" +
+                $"Phone Number = {PhoneNumber} \n" +
+                $"Product Category = {ProductСategory} \n" +
+                $"Installment Range = {InstallmentRange} \n" +
+                $"Product Cost = {summa}";
+            smsSender.Send();
+        }
     }
 }
