b2435c3492acdb5565888b257886d7af3eda46c1
Firuz <razackoff@gmail.com>
2023-03-10 11:17:35 +0500

    Добавить обработчик ошибок.

---
+++ Task for Junior CSharp Developer/Models/SmsSender.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Task_for_Junior_CSharp_Developer.Models
+{
+    public class SmsSender
+    {
+        public string PhoneNumber { get; set; }
+        public string Sms { get; set; }
+
+        public void Send()
+        {
+            return;
+        }
+    }
+}
