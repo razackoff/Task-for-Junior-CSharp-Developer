4bcc0b1aac708d58f5d30695f07cd44302e50c91
Firuz <razackoff@gmail.com>
2023-03-10 11:18:14 +0500

    Revert "Revert "Добавить обработчик ошибок.""
    This reverts commit d23458cb2a989723c2e98eebdf16edab0d294f78.

---
+++ Task for Junior CSharp Developer/Models/InstallmentHandler.cs
@@ -0,0 +1,71 @@
+﻿using Market.Application.Common.Exceptions;
+
+namespace Task_for_Junior_CSharp_Developer.Models
+{
+    public class InstallmentHandler
+    {
+        public string productСategory { get; set; }
+        public decimal price { get; set; }
+        private decimal summa;
+        public decimal installmentRange { get; set; }
+        private decimal installmentProcent = 0;
+        
+
+        public decimal GetInstallment(decimal price) 
+        {
+            if(installmentRange == 3)
+            {
+                installmentRange = 1;
+            }
+            else if(installmentRange == 6) 
+            {
+                installmentRange = 2;
+            }
+            else if(installmentRange == 9)
+            {
+                installmentRange = 3;
+            }
+            else if(installmentRange == 12)
+            {
+                installmentRange = 4;
+            }
+            else if(installmentRange == 18)
+            {
+                installmentRange = 5;
+            }
+            else if(installmentRange == 24)
+            {
+                installmentRange = 6;
+            }
+            else
+            {
+                throw new NotFoundException();
+            }
+            if(productСategory == "Smartphone")
+            {
+                if(installmentRange > 3)
+                {
+                    installmentProcent = 3 * installmentRange - 3;
+                }
+
+            }
+            else if(productСategory == "Computer")
+            {
+                if (installmentRange > 4)
+                {
+                    installmentProcent = 4 * installmentRange - 4;
+                }
+            }
+            else if(productСategory == "TV")
+            {
+                if (installmentRange > 5)
+                {
+                    installmentProcent = 5 * installmentRange - 5;
+                }
+            }
+            summa = price;
+            summa = price / 100 * installmentProcent;
+            return summa;
+        }
+    }
+}
--- Task for Junior CSharp Developer/Models/PurchaseTransactionDto.cs
+++ Task for Junior CSharp Developer/Models/PurchaseTransactionDto.cs
@@ -4,7 +4,7 @@ namespace Task_for_Junior_CSharp_Developer.Models
 {
     public class PurchaseTransactionDto
     {
-        public string productName { get; set; }
+        public string productСategory { get; set; }
         public decimal productCost { get; set; }
         public string phoneNumber { get; set; }
         public int installmentRange { get; set; }
