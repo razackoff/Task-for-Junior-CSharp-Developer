6217064164f7cd54f8bdff83712d91244167c043
Firuz <razackoff@gmail.com>
2023-03-10 10:01:29 +0500

    Создать класс объекта.

---
+++ Task for Junior CSharp Developer/Models/PurchaseTransaction.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_for_Junior_CSharp_Developer.Models
+{
+    public class PurchaseTransaction
+    {
+        public Guid TransactionID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Status { get; set; }
+        public string ProductName { get; set; }
+        public int InstallmentRange { get; set; }
+        public decimal ProductCost { get; set; }
+
+    }
+}
